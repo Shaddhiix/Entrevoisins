1c17bb79982d85ad73e9a8f2720fe389ed176902
Shaddhiix <leonard.alexandre17@gmail.com>
2022-02-16 13:26:08 +0100

    Profil

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/profile/Profile.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.entrevoisins.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.openclassrooms.entrevoisins.R;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
